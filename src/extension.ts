4e9650b898228bdde5ccdf73fded86235b96d60c
Zhang Kaiyu <yeannylam@gmail.com>
2019-05-30 19:45:00 +0800

    switch to last editor

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { Disposable, ExtensionContext, commands, window, TextEditor, ViewColumn } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let lastBufferDispose: Disposable | undefined;
+let lastEditor: TextEditor | undefined;
+let currentEditor: TextEditor | undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "window-switch" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+export function activate(context: ExtensionContext) {
+	lastBufferDispose = window.onDidChangeActiveTextEditor((editor) => {
+		if (editor === currentEditor) {
+			return;
+		}
+		lastEditor = currentEditor;
+		currentEditor = editor;
 	});
 
+	let disposable = commands.registerCommand('windowSwitch.switchToLastOpenedEditor', () => {
+		if (lastEditor) {
+			window.showTextDocument(lastEditor.document, ViewColumn.Active, false);
+		}
+	});
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	(<Disposable>lastBufferDispose).dispose();
+}
