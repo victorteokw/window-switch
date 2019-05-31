5541945d73bc0cd2dce62b9ff4431bb19842297f
Zhang Kaiyu <yeannylam@gmail.com>
2019-05-31 17:37:19 +0800

    add close commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,17 +6,48 @@ import TextDocumentItem from "./TextDocumentItem";
 
 let lastEditorDisposable: Disposable | undefined;
 let lastFileDisposable: Disposable | undefined;
+let lastFileDisposable2: Disposable | undefined;
 let lastDocument: TextDocument | undefined;
 let currentDocument: TextDocument | undefined;
+let openedDocuments: TextDocument[] = [];
 
 export function activate(context: ExtensionContext) {
 
+  workspace.textDocuments.forEach((doc) => {
+		if (!shouldIgnoreDocument(doc)) {
+			openedDocuments.push(doc);
+		}
+	});
+
 	lastEditorDisposable = window.onDidChangeActiveTextEditor((editor) => {
 		if (editor) {
+			if (editor.document) {
+				if (!shouldIgnoreDocument(editor.document)) {
+					const index = openedDocuments.indexOf(editor.document);
+					if (index > -1) {
+						openedDocuments.splice(index, 1);
+					}
+					openedDocuments.unshift(editor.document);
+				}
+			}
 			if (currentDocument !== editor.document) {
 				lastDocument = currentDocument;
 				currentDocument = editor.document;
 			}
+		}
+	});
+
+	lastFileDisposable = workspace.onDidOpenTextDocument((textDocument) => {
+		if (shouldIgnoreDocument(textDocument)) return;
+		if (openedDocuments.indexOf(textDocument) === -1) {
+			openedDocuments.unshift(textDocument);
+		}
+	});
+
+	lastFileDisposable2 = workspace.onDidCloseTextDocument((textDocument) => {
+		const index = openedDocuments.indexOf(textDocument);
+		if (index > -1) {
+			openedDocuments.splice(index, 1);
 		}
 	});
 
@@ -35,13 +66,16 @@ export function activate(context: ExtensionContext) {
 		let selected: TextDocumentItem | undefined;
 		const quickPick = window.createQuickPick<TextDocumentItem>();
 		quickPick.title = "Opened Files";
-		const items: TextDocumentItem[] = [];
 		quickPick.matchOnDescription = true;
 		quickPick.matchOnDetail = true;
-		workspace.textDocuments.forEach((doc) => {
-			if (shouldIgnoreDocument(doc)) return;
-			items.push(new TextDocumentItem(doc));
-		});
+		const items: TextDocumentItem[] = [];
+		for (const doc of openedDocuments) {
+			if (window.activeTextEditor && (doc === window.activeTextEditor.document)) {
+				// just ignore
+			} else {
+				items.push(new TextDocumentItem(doc));
+			}
+		}
 		quickPick.onDidChangeSelection((e: TextDocumentItem[]) => {
 			selected = e[0];
 		});
@@ -51,15 +85,29 @@ export function activate(context: ExtensionContext) {
 			}
 		});
 		quickPick.items = items;
-		quickPick.buttons = [{
-			iconPath: ThemeIcon.File,
-			tooltip: 'Learn More'
-	}];
 		quickPick.show();
 	});
+	context.subscriptions.push(disposable);
+
+	disposable = commands.registerCommand('windowSwitch.saveFileInActiveEditor', () => {
+		commands.executeCommand('workbench.action.files.save');
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = commands.registerCommand('windowSwitch.saveAllFiles', () => {
+		commands.executeCommand('workbench.action.files.saveAll');
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = commands.registerCommand('windowSwitch.closeCurrentEditor', () => {
+		commands.executeCommand('workbench.action.closeActiveEditor');
+	});
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
 	(<Disposable>lastEditorDisposable).dispose();
+	(<Disposable>lastFileDisposable).dispose();
+	(<Disposable>lastFileDisposable2).dispose();
 }
